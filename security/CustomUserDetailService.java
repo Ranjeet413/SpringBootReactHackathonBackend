d22aa7ffe8c00167ff95f66b1cba48ea8fc0a599
Ranjeet413 <53113770+Ranjeet413@users.noreply.github.com>
2022-11-02 16:15:28 +0530

    security

---
+++ security/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package com.ranjeet.blog.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ranjeet.blog.entities.User;
+import com.ranjeet.blog.exceptions.ResourceNotFoundException;
+import com.ranjeet.blog.repositories.UserRepo;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		// loading user from database by username
+		User user = this.userRepo.findByEmail(username)
+				.orElseThrow(() -> new ResourceNotFoundException("User ", " email : " + username, 0));
+
+		return user;
+	}
+
+}
