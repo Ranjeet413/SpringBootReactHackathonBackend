72f19aedce411bd1df439d42e1c00174d6a540eb
Ranjeet413 <53113770+Ranjeet413@users.noreply.github.com>
2022-11-02 16:15:57 +0530

    services

---
+++ services/UserService.java
@@ -0,0 +1,23 @@
+package com.ranjeet.blog.services;
+
+
+import java.util.List;
+
+import com.ranjeet.blog.payloads.UserDto;
+
+public interface UserService {
+
+	UserDto registerNewUser(UserDto user);
+	
+	
+	UserDto createUser(UserDto user);
+
+	UserDto updateUser(UserDto user, Integer userId);
+
+	UserDto getUserById(Integer userId);
+
+	List<UserDto> getAllUsers();
+
+	void deleteUser(Integer userId);
+
+}
