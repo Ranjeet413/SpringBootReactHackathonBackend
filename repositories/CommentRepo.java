e6e0249d4bc77e2489ac471a38c2cf417fa5f157
Ranjeet413 <53113770+Ranjeet413@users.noreply.github.com>
2022-11-22 19:57:21 +0530

    Delete CommentRepo.java

--- repositories/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ranjeet.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ranjeet.blog.entities.Comment;
-
-public interface CommentRepo  extends JpaRepository<Comment	, Integer> {
-
-}
